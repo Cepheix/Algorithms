9faa9a5cea962d2833c35334c6bb5037993cc497
Piotr Ptak <piotrptak@outlook.com>
2017-03-21 23:05:31 +0100

    Dodano kolejne implementacje sortowania

--- Algorithms/Algorithms/Soring.cs
+++ Algorithms/Algorithms/Soring.cs
@@ -47,6 +47,52 @@ namespace Algorithms.Algorithms
             }
         }
 
+        /// <summary>
+        /// Dla każdego elementu sprawdź wszystkie elementy po jego lewej stronie i jeśli są w niepoprawnej kolejności zamień je ze sobą
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        public static void InsertionSort<T>(IList<T> collection) where T : IComparable<T>
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (collection[j].CompareTo(collection[j-1]) > 0)
+                    {
+                        Swap(collection, j, j - 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Wykonaj sortowanie przez wstawianie ale nie co 1 tylko co jakąś dużą wartość a następnie zmniejszaj ją aż do 1
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        public static void ShellSort<T>(IList<T> collection) where T : IComparable<T>
+        {
+            // knuth 3x + 1 sequence
+            int h = 1;
+            while (h >= 1)
+            {
+                for (int i = h; i < collection.Count; i++)
+                {
+                    for (int j = i; j >= h && collection[j].CompareTo(collection[j - h]) > 0; j -= h)
+                    {
+                        Swap(collection, j, j - h);
+                    }
+                }
+
+                h = h / 3;
+            }
+        }
+
         public static bool IsSorted<T>(IList<T> collection) where T : IComparable<T>
         {
             for (int i = 1; i < collection.Count; i++)
@@ -57,6 +103,21 @@ namespace Algorithms.Algorithms
                 }
             }
             return true;
+        }
+
+        /// <summary>
+        /// Ustawia elementy w losowej kolejności
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        public static void Shuffle<T>(IList<T> collection)
+        {
+            Random random = new Random();
+            for (int i = 0; i < collection.Count; i++)
+            {
+                int r = random.Next(0, i - 1);
+                Swap(collection, i, r);
+            }
         }
 
         private static void Swap<T>(IList<T> collection, int index1, int index2)
