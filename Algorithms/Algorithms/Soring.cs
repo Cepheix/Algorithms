d2b2fdcbc88013876e0d7f6a73a53f4cf34c321e
Piotr Ptak <piotrptak@outlook.com>
2017-03-29 14:52:39 +0200

    Dodano sortowanie szybkie

--- Algorithms/Algorithms/Soring.cs
+++ Algorithms/Algorithms/Soring.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithms.Algorithms
 {
     public static class Soring
     {
+
+        private const int CUT_OFF = 7;
 
         /// <summary>
         /// Znajdź najmniejszy element na prawo od aktualnego indeksu, zamień go z nim i zwiększ indeks, powtórz dla wszystkich
@@ -23,22 +21,6 @@ namespace Algorithms.Algorithms
                 for (int j = i + 1; j < collection.Count; j++)
                 {
                     if (collection[j].CompareTo(collection[min]) < 0)
-                    {
-                        min = j;
-                    }
-                }
-                Swap(collection, i, min);
-            }
-        }
-
-        public static void SelectionSort<T>(IList<T> collection, Func<T, T, bool> compareFunction)
-        {
-            for (int i = 0; i < collection.Count; i++)
-            {
-                int min = i;
-                for (int j = i + 1; j < collection.Count; j++)
-                {
-                    if (compareFunction(collection[j], collection[min]))
                     {
                         min = j;
                     }
@@ -93,6 +75,35 @@ namespace Algorithms.Algorithms
             }
         }
 
+        /// <summary>
+        /// Sortowanie przez scalanie
+        /// Dzieli kolekcję na dwie części, rekursyjne sortuje każdą z nich a następnie łączy je
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        public static void MergeSort<T>(IList<T> collection) where T : IComparable<T>
+        {
+            T[] auxiliary = new T[collection.Count];
+
+            MergeSort(collection, auxiliary, 0, collection.Count - 1);
+        }
+
+        /// <summary>
+        /// Sortowanie szybkie
+        /// Potasuj losowo kolekcję,
+        /// podziel na dwie części a następnie posortuj każdą z części rekurencyjnie
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        public static void QuickSort<T>(IList<T> collection) where T : IComparable<T>
+        {
+            // potasuj losowo tablicę
+            Shuffle(collection);
+
+            // posortuj tablicę
+            QuickSort(collection, 0, collection.Count - 1);
+        }
+
         public static bool IsSorted<T>(IList<T> collection) where T : IComparable<T>
         {
             for (int i = 1; i < collection.Count; i++)
@@ -127,9 +138,150 @@ namespace Algorithms.Algorithms
             collection[index2] = swap;
         }
 
-        private static bool Less(IComparable item1, IComparable item2)
+        #region MergeSort
+
+
+        private static void MergeSort<T>(IList<T> collection, IList<T> auxiliaryCollection, int low, int high) where T : IComparable<T>
         {
-            return item1.CompareTo(item2) < 0;
+            // jeżeli elementów jest mało to posrotuj przez wstawianie
+            if (high <= low + CUT_OFF - 1)
+            {
+                InsertionSort(collection);
+                return;
+            }
+
+            // podziel kolekcję na dwie części w połowie
+            int mid = low + ((high - low) / 2);
+
+            // posortuj pierwszą połowę
+            MergeSort(collection, auxiliaryCollection, low, mid);
+            // posortuj drugą połowę
+            MergeSort(collection, auxiliaryCollection, mid + 1, high);
+
+            // jeżeli ostatni element w pierwszej części jest mniejszy od pierwszego elementu w drugiej części to zakończ
+            if (collection[mid].CompareTo(collection[mid + 1]) < 0)
+            {
+                return;
+            }
+            
+            // złącz obydwie części
+            Merge(collection, auxiliaryCollection, low, mid, high);
         }
+
+        private static void Merge<T>(IList<T> collection, IList<T> auxuiliaryCollection, int low, int mid, int high) where T : IComparable<T>
+        {
+            // założenie kolekcja w przedziale low do mid jest posortowana i mid + 1 do high też jest posortowana
+
+            // skopiuj zawartość sortowanej tabeli do tabeli pomocniczej
+            for (int k = 0; k < collection.Count; k++)
+            {
+                auxuiliaryCollection[k] = collection[k];
+            }
+
+            // wskaźnik do pierwszej części kolekcji
+            int i = low;
+            // wskaźnik do drugiej części kolekcji
+            int j = mid + 1;
+
+            for (int k = low; k < high; k++)
+            {
+                // jeśli w pierwszej części już nie ma elementów weź z drugiej części i zwiększ jej wskaźnik 
+                if (i > mid)
+                {
+                    collection[k] = auxuiliaryCollection[j++];
+                }
+                // jeśli w drugiej części już nie ma elementów weź z pierwszej części i zwiększ jej wskaźnik
+                else if (j > high)
+                {
+                    collection[k] = auxuiliaryCollection[i++];
+                }
+                // jeśli element w drugiej części jest większy od elementu w pierwszej części weź go i zwiększ wskaźnik drugiej części
+                else if (auxuiliaryCollection[j].CompareTo(auxuiliaryCollection[i]) > 0)
+                {
+                    collection[k] = auxuiliaryCollection[j++];
+                }
+                // to znaczy, że element w pierwszej części jest większy niż ten w drugiej części, weź go i zwiększ wskaźnik dla pierwszej części
+                else
+                {
+                    collection[k] = auxuiliaryCollection[i++];
+                }
+            }
+        }
+
+        #endregion
+
+        #region QuickSort
+
+        private static void QuickSort<T>(IList<T> collection, int low, int high) where T : IComparable<T>
+        {
+            if (high <= low)
+            {
+                return;
+            }
+
+            // podziel tablicę w miejscu j, tak że wszystkie elementy na lewo od niego są od niego mniejsze lub równe
+            // a na prawo są większe lub równe
+            int j = Partition(collection, low, high);
+
+            // posortuj rekurencyjnie lewą część
+            QuickSort(collection, low, j - 1);
+
+            // posortuj rekurencyjnie prawą część
+            QuickSort(collection, j + 1, high);
+        }
+
+        /// <summary>
+        /// Dzieli kolekcję na dwie części w miejscu j, tak że wszystkie elementy na lewo od niego są od niego mniejsze lub równe
+        /// a na prawo od niego są większe lub równe
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        private static int Partition<T>(IList<T> collection, int low, int high) where T : IComparable<T>
+        {
+            // zamieniaj miejscami elementy jeżeli te po prawej stronie są mniejsze niż low lub gdy te po lewej stronie są większe niż low
+            // na samym końcu zmień element low z elementem pod indeksem j
+
+            int i = low;
+            int j = high + 1;
+            while (true)
+            {
+                // dopóki elementy na lewo od miejsca low są od niego mniejsze w porządku
+                while (collection[++i].CompareTo(collection[low]) < 1)
+                {
+                    if (i == high)
+                    {
+                        break;
+                    }
+                }
+
+                // dopóki elementy na prawo od miejsca low są od niego większe w porządku
+                while (collection[--j].CompareTo(collection[low]) > 1)
+                {
+                    if (j == low)
+                    {
+                        break;
+                    }
+                }
+
+                // jeżeli wskaźniki i oraz j się przecięły zakończ
+                if (i >= j)
+                {
+                    break;
+                }
+
+                // zamień elementy i oraz j miejscami ponieważ znajdują się w niewłaściwych częsciach
+                Swap(collection, i, j);
+            }
+
+            // zamień element low na nowy element podziału znajdujący się na indeksie j
+            Swap(collection, low, j);
+            return j;
+        }
+
+        #endregion
+
     }
 }
