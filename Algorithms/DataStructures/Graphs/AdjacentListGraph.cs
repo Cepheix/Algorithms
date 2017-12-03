bc730985d2eb7b76f69d03a28202925b4db37637
Piotr Ptak <piotrptak@outlook.com>
2017-12-03 19:39:09 +0100

    added adjacent list representation of graph

---
+++ Algorithms/DataStructures/Graphs/AdjacentListGraph.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures.Graphs
+{
+    public class AdjacentListGraph : Graph
+    {
+        private readonly int _v;
+        private List<int>[] _adj;
+
+        public AdjacentListGraph(int V) : base(V)
+        {
+            this._v = V;
+            this._adj = new List<int>[V];
+
+            for (int v = 0; v < V; v++)
+            {
+                this._adj[v] = new List<int>();
+            }
+        }
+
+        public override void AddEdge(int v, int w)
+        {
+            this._adj[v].Add(w);
+            this._adj[w].Add(v);
+        }
+
+        public override IEnumerable<int> Adjacent(int v)
+        {
+            return this._adj[v];
+        }
+
+        public override int E()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int V()
+        {
+            return this._v;
+        }
+    }
+}
