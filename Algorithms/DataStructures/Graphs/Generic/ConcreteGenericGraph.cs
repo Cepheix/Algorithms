9c44f5ef54ac26ceeeccfb2cc4ae98d183e963a8
Piotr Ptak <piotrptak@outlook.com>
2017-12-03 19:39:26 +0100

    added my implementation of generic graph

---
+++ Algorithms/DataStructures/Graphs/Generic/ConcreteGenericGraph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures.Graphs.Generic
+{
+    public class ConcreteGenericGraph<T> : GenericGraph<T>
+    {
+        private Graph _graph;
+        private Dictionary<T, int> _matchingTable;
+
+        public ConcreteGenericGraph(int V) : base(V)
+        {
+            this._matchingTable = new Dictionary<T, int>();
+            this._graph = new AdjacentListGraph(V);
+        }
+
+        public override void AddEdge(T v, T w)
+        {
+            this._graph.AddEdge(_matchingTable[v], _matchingTable[w]);
+        }
+
+        public override IEnumerable<T> Adjacent(T v)
+        {
+            //List<T> result = new List<T>();
+
+            //foreach (int x in this._graph.Adjacent(_matchingTable[v]))
+            //{
+            //    result.Add(_matchingTable[x]);
+            //}
+
+            throw new NotImplementedException();
+        }
+
+        public override int E()
+        {
+            return this._graph.E();
+        }
+
+        public override int V()
+        {
+            return this._graph.V();
+        }
+    }
+}
---
+++ Algorithms/DataStructures/Graphs/Generic/GenericGraph.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures.Graphs.Generic
+{
+    public abstract class GenericGraph<T>
+    {
+        /// <summary>
+        /// Create an empty graph with V verivcies
+        /// </summary>
+        /// <param name="V"></param>
+        public GenericGraph(int V)
+        {
+        }
+
+        /// <summary>
+        /// Add an edge connecting verticies v-w
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="w"></param>
+        public abstract void AddEdge(T v, T w);
+
+        /// <summary>
+        /// Get vericies adjacent to v
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public abstract IEnumerable<T> Adjacent(T v);
+
+        /// <summary>
+        /// Number of vericies
+        /// </summary>
+        /// <returns></returns>
+        public abstract int V();
+
+        /// <summary>
+        /// Number of edges
+        /// </summary>
+        /// <returns></returns>
+        public abstract int E();
+
+        /// <summary>
+        /// String representation
+        /// </summary>
+        /// <returns></returns>
+        //public override string ToString()
+        //{
+        //    StringBuilder result = new StringBuilder();
+
+        //    for (int v = 0; v < V(); v++)
+        //    {
+        //        foreach (T w in Adjacent(v))
+        //        {
+        //            result.Append(String.Format("{0} - {1}", v, w));
+        //        }
+        //    }
+
+        //    return result.ToString();
+        //}
+    }
+}
