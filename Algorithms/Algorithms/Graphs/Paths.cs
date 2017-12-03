936e8a409a6838a2d55b12a2815f9e2c50e470f6
Piotr Ptak <piotrptak@outlook.com>
2017-12-03 21:03:25 +0100

    added Paths base class

---
+++ Algorithms/Algorithms/Graphs/Paths.cs
@@ -0,0 +1,35 @@
+﻿using Algorithms.DataStructures.Graphs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Algorithms.Graphs
+{
+    public abstract class Paths
+    {
+        /// <summary>
+        /// find paths in graph from source s
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <param name="s"></param>
+        public Paths(Graph graph, int s)
+        { }
+
+        /// <summary>
+        /// Is there a path from s to v
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public abstract bool HasPathTo(int v);
+
+        /// <summary>
+        /// Paths from s to v 
+        /// null if there is no path
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public abstract IEnumerable<int> PathTo(int v);
+    }
+}
