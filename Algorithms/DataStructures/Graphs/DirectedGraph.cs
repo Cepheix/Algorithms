db3b7b08baefdeb6dba4242d7430f119a6de6998
Piotr Ptak <piotrptak@outlook.com>
2017-12-04 19:24:00 +0100

    added DirectedGraph class

---
+++ Algorithms/DataStructures/Graphs/DirectedGraph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures.Graphs
+{
+    public abstract class DirectedGraph : Graph
+    {
+        public DirectedGraph(int V) : base(V)
+        {
+        }
+
+        public abstract DirectedGraph Reverse();
+    }
+}
