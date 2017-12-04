2ce04b41fee3e2df20534c4fa60845e8d9b505ff
Piotr Ptak <piotrptak@outlook.com>
2017-12-04 21:31:27 +0100

    added minimum spanning tree Api

---
+++ Algorithms/Algorithms/Graphs/MinimumSpanningTree.cs
@@ -0,0 +1,13 @@
+﻿using Algorithms.DataStructures.Graphs;
+using System.Collections.Generic;
+
+namespace Algorithms.Algorithms.Graphs
+{
+    public abstract class MinimumSpanningTree
+    {
+        MinimumSpanningTree(EdgeWeightedAbstractGraph graph) { }
+
+        public abstract IEnumerable<Edge> Edges();
+        public abstract double Weight();
+    }
+}
