27d8279f6f285ec0079f75d42b1231b55c7d8b78
Piotr Ptak <piotrptak@outlook.com>
2017-12-04 21:00:51 +0100

    added weighted edge representation

---
+++ Algorithms/DataStructures/Graphs/AbstractEdge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithms.DataStructures.Graphs
+{
+    /// <summary>
+    /// weighted edge v-w
+    /// </summary>
+    public abstract class AbstractEdge : IComparable<AbstractEdge>
+    {
+        public AbstractEdge(int v, int w, double weight)
+        { }
+
+        /// <summary>
+        /// either endpoint
+        /// </summary>
+        /// <returns></returns>
+        public abstract int Either();
+
+        /// <summary>
+        /// the endpoint that's not v
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public abstract int Other(int v);
+
+        public abstract int CompareTo(AbstractEdge other);
+
+        /// <summary>
+        /// the weight
+        /// </summary>
+        /// <returns></returns>
+        public abstract double Weight();
+    }
+}
---
+++ Algorithms/DataStructures/Graphs/Edge.cs
@@ -0,0 +1,43 @@
+﻿namespace Algorithms.DataStructures.Graphs
+{
+    public class Edge : AbstractEdge
+    {
+        private readonly int _v;
+        private readonly int _w;
+        private readonly double _weight;
+
+        public Edge(int v, int w, double weight) : base(w, v, weight)
+        {
+            this._v = v;
+            this._w = w;
+            this._weight = weight;
+        }
+
+        public override int CompareTo(AbstractEdge other)
+        {
+            if (this._weight < other.Weight())
+                return -1;
+            else if (this._weight > other.Weight())
+                return +1;
+            else return 0;
+        }
+
+        public override int Either()
+        {
+            return _v;
+        }
+
+        public override int Other(int vertex)
+        {
+            if (vertex == _v)
+                return _w;
+            else
+                return _v;
+        }
+
+        public override double Weight()
+        {
+            return _weight;
+        }
+    }
+}
